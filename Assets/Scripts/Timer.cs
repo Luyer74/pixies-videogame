246b844f3f475f64b3a1336f453687b850ff0a46
OUMontiel <omarume@gmail.com>
2020-05-24 20:16:16 -0700

    Add Pause button and change Timer UI

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private Text timerText = null;
+    [SerializeField] private TMP_Text timerText = null;
     [SerializeField] private Slider slider = null;
    
 
