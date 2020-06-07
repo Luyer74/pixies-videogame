0550364c7eef9a7f022772a83fb6f3ea051fe770
Luyer74 <a00825080@itesm.mx>
2020-06-06 23:05:05 -0500

    add time bar to cutting stations

---
+++ Assets/Scripts/CutIngredient.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CutIngredient : MonoBehaviour
+{
+    public GameObject imagen;
+    Image timeBar;
+    public bool isInRange;
+    public KeyCode intkey;
+    PlayerInteract cutStation;
+    GameObject player;
+    GameObject ingrediente;
+    public float startTime = 0f; //tiempo de inicio de presionar tecla
+    public float timer = 0f;
+    public float holdTime = 5.0f; //tiempo requerido para "cortar"
+    public bool held = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        player = collision.gameObject;
+        ingrediente = player.transform.GetChild(0).gameObject;
+        cutStation = collision.GetComponent<PlayerInteract>(); //obtener objeto para usar las funciones de playerinteract
+        if (cutStation)
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+
+    private void Start()
+    {
+        imagen.SetActive(false);
+        timeBar = imagen.GetComponent<Image>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            if (ingrediente)
+            {
+                if (Input.GetKeyDown(intkey))
+                {
+
+                    ingrediente.transform.position = ingrediente.transform.position + new Vector3(1, 0);
+
+                    imagen.SetActive(true);
+                    startTime = Time.time;
+                    timer = startTime;
+
+                }
+
+
+                if (Input.GetKey(intkey) && held == false)
+                {
+                    timer += Time.deltaTime;
+                    timeBar.fillAmount = (timer - startTime) / holdTime; //llenar barra de tiempo mientras se siga presionando
+                    if (timer > (startTime + holdTime))
+                    {
+                        held = true;
+                        cutStation.useIngredient(gameObject);
+                    }
+                }
+            }
+
+            if (Input.GetKeyUp(intkey))
+            {
+                if (ingrediente)
+                {
+                    ingrediente.transform.position = ingrediente.transform.position + new Vector3(-1, 0);
+                }
+                
+                timeBar.fillAmount = 0; //barra vacia
+                held = false;
+                imagen.SetActive(false);
+            }
+        }
+    }
+}
--- Assets/Scripts/GetIngredient.cs
+++ Assets/Scripts/GetIngredient.cs
@@ -8,10 +8,7 @@ public class GetIngredient : MonoBehaviour
     public bool isInRange; //esta en rango con alguhn objeto
     public KeyCode intkey; // tecla "E"
     PlayerInteract player1; //script para interactuar con objetos
-    public float startTime = 0f; //tiempo de inicio de presionar tecla
-    public float timer = 0f;
-    public float holdTime = 5.0f; //tiempo requerido para "cortar"
-    public bool held = false; //
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         player1 = collision.GetComponent<PlayerInteract>(); //obtener objeto para usar las funciones de playerinteract
@@ -35,25 +32,7 @@ public class GetIngredient : MonoBehaviour
         {
             if (Input.GetKeyDown(intkey))
             {
-                startTime = Time.time;
-                timer = startTime;
                 player1.getIngredient(gameObject); //llamar a player interact con el objeto con el que se interactua
-            }
-
-            if(Input.GetKey(intkey) && held == false)
-            {
-                timer += Time.deltaTime;
-
-                if(timer > (startTime + holdTime))
-                {
-                    held = true;
-                    player1.useIngredient(gameObject);
-                }
-            }
-
-            if (Input.GetKeyUp(intkey))
-            {
-                held = false;
             }
         }
     }
