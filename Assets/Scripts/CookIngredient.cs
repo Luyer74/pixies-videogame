9c927306bbeb36cec4ad02cf26f2895d10f6e907
Luyer74 <a00825080@itesm.mx>
2020-06-10 18:54:34 -0500

    barras de tiempo en cookpots

--- Assets/Scripts/CookIngredient.cs
+++ Assets/Scripts/CookIngredient.cs
@@ -8,15 +8,16 @@ public class CookIngredient : MonoBehaviour
     public GameObject imagen; //barra de tiempo
     Image timeBar;
     public float timer = 0f;
-    public float holdTime = 15.0f; //tiempo requerido para cocinar
+    public float holdTime = 20.00f; //tiempo requerido para cocinar
     public float startTime = 0f;
     public bool isInRange; //esta en rango con alguhn objeto
     public KeyCode intkey; // tecla "E"
     PlayerInteract player1; //script para interactuar con objetos
     GameObject player;
     GameObject fire;
-    int ingredientCount;
+    public int ingredientCount = 0; //ingredientes en uso
     public ParticleSystem particles;
+    bool isCooking = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -38,19 +39,45 @@ public class CookIngredient : MonoBehaviour
 
     private void Update()
     {
-        if (isInRange)
+        if (isInRange && ingredientCount < 3)
         {
             if (Input.GetKeyDown(intkey) && player1.hasObject)
             {
-                fire = gameObject.transform.GetChild(0).gameObject;
-                fire.SetActive(true);
-                particles = fire.GetComponent<ParticleSystem>();
-                if (!particles.isPlaying)
+                if (!isCooking)
                 {
+                    imagen.SetActive(true);
+                    startTime = Time.time;
+                    timer = startTime;
+                    isCooking = true;
+                    fire = gameObject.transform.GetChild(0).gameObject;
+                    fire.SetActive(true);
+                    particles = fire.GetComponent<ParticleSystem>();
                     particles.Play();
                 }
+                ingredientCount++;
                 player1.cook(gameObject); //llamar a player interact con el objeto con el que se interactua
             }
         }
+
+        if (isCooking)
+        {
+            timer += Time.deltaTime;
+            timeBar.fillAmount = 1 - ((timer - startTime) / holdTime);
+        }
+
+        if (timer > (startTime + holdTime))
+        {
+            imagen.SetActive(false);
+            timeBar.fillAmount = 1; //barra vacia
+            ingredientCount = 0;
+            isCooking = false;
+            fire.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        imagen.SetActive(false);
+        timeBar = imagen.GetComponent<Image>();
     }
 }
